f80bc8bae1fe53b091d47585c542b01fa27067c9
lthanhbinh <lthanhbinh@tma.com.vn>
2018-03-14 23:56:49 +0700

    v1

---
+++ header.php
@@ -0,0 +1,60 @@
+<!-- Navigation -->
+<header>
+    <nav class="navbar navbar-default navbar-fixed-top ">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#"><img src="images/logos/logo.png"/></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">MEM OIRS</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">ABOUT</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">SHOP</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">BANGERS</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">TALK TO ME</a>
+                    </li>
+                    <li>
+                        <a class="cartIcon" href="#"></a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <div class="signUpFree navbar-fixed-top">
+        <div class="container">
+            <span class="txt">SIGN UP FOR 10% OFF YOUR FIRST ORDER</span>
+            <div class="signupBox">
+                <form action="" method="">
+                    <div class="orangeBox">
+                        <span class="title">EMAIL |</span>
+                        <input type="text" value="" placeholder="" />
+                    </div>
+                    <input type="submit" class="btn-button" value="SIGN UP"/>
+                </form>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ js/class.SiteMain.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+var SiteMain = (function() {
+	function init(){
+	}
+
+	function createRadio(){
+		$('input.iCheckRadio').iCheck();
+	}
+
+	function openPopup(idDiv){
+		$('.result_question').css('display','none')
+		$(idDiv).css('display','block');
+	}
+	function closePopup(idDiv){
+		$(idDiv).css('display','none');
+	}
+	return {
+		init:init,
+		openPopup:openPopup,
+		closePopup:closePopup
+	}
+
+})();
+
+$(document).ready( function() {
+	SiteMain.init();
+});

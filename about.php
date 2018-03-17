00df71ff539e8d64a116dde13f0adca608d284ba
QuangSang <quangsang222@gmail.com>
2018-03-17 22:32:23 +0700

    4 pages

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php include('header.php') ?>
+<section class="homepage aboutpage paddingBody commonpage">
+  <h2>ABOUT US</h2>
+  <div class="container">
+    <div class="row marginbottom">
+      <div class="col-md-4 col-sm-5 col-xs-12">
+        <img src="images/portrait_person.png" alt="">
+      </div>
+      <div class="col-md-8 col-sm-7 col-xs-12">
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore
+        eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore
+        eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore
+        eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+      </div>
+    </div>
+  </div>
+  <img src="images/banner.jpg" class="img-responsive img-bottom" alt="">
+</section>
+<?php include ('footer.php') ?>
---
+++ products.php
@@ -0,0 +1,94 @@
+<?php include('header.php') ?>
+<section class="homepage memoirs_page products_page paddingBody commonpage">
+  <h2>COMMEN CLOTHING</h2>
+  <div class="container">
+    <div class="row">
+      <div class="section-filter__main">
+        <div class="section-filter__main--nav">
+          <a href="#">All</a>
+          <a href="#">Tee</a>
+          <a href="#">Shorts</a>
+          <a href="#">Accesories</a>
+        </div>
+        <div class="section-filter__main--content clearfix">
+
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+          <!-- item -->
+          <div class="products_page-item col-md-3 col-sm-4 col-xs-6">
+            <img src="images/product1.jpg" class="img-responsive" alt="">
+            <p>TEE NAME</p>
+            <span>£29.99</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php include ('footer.php') ?>

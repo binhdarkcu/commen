5e9cdfac13a178f54d31489df203d9879b750329
lthanhbinh <lthanhbinh@tma.com.vn>
2018-03-17 16:40:19 +0700

    contact page

--- contact.php
+++ contact.php
@@ -36,11 +36,28 @@
 
 <body class="index">
     <?php include('header.php')?>
-    <div class="contactPage paddingBody">
-        <div class="row">
-            <div class="container">
-                aaa
-            </div>
+    <div class="contactPage paddingBody section-filter">
+        <div class="contactForm">
+            <h3>Hey there, what can <br/>we help you with today?</h3>
+            <form method="" action="">
+                <div class="contactRow">
+                    <label>Name |</label>
+                    <input type="text" value="" name="username" />
+                </div>
+                <div class="contactRow">
+                    <label>Email |</label>
+                    <input type="text" value="" name="email" />
+                </div>
+                <div class="contactRow subject">
+                    <label>What shall we chat about |</label>
+                    <input type="text" value="" name="subject" />
+                </div>
+                <div class="contactRow message">
+                    <label>Message |</label>
+                    <textarea></textarea>
+                </div>
+                <input type="submit" value="Submit"/>
+            </form>
         </div>
     </div>
     <!-- Loader -->
